1733ca2e61a12f92d5d4c44213b2992e9e290a3c
bhanuchander reddy <bhanuchanderreddy786@gmail.com>
2020-08-14 15:03:41 +0530

    WishlistActivity is implemented

---
+++ app/src/main/java/grocery/app/WishlistActivity.java
@@ -0,0 +1,51 @@
+package grocery.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+public class WishlistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wishlist);
+        ListView listView = findViewById(R.id.listView);
+        ListViewAdapter listViewAdapter = new ListViewAdapter();
+        listView.setAdapter(listViewAdapter);
+    }
+
+    public void onBackClick(View view) {
+        finish();
+    }
+
+    class ListViewAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            if(view == null)
+                view = LayoutInflater.from(WishlistActivity.this).inflate(R.layout.wishlist_layout,viewGroup,false);
+            return view;
+        }
+    }
+}
