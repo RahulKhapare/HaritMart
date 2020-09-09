b69514639bf72fd80b1912ad6092b7e30dd76bcb
bhanuchander reddy <bhanuchanderreddy786@gmail.com>
2020-09-09 17:18:30 +0530

    New Address Screen is implemented

--- app/src/main/java/grocery/app/MyAccountActivity.java
+++ app/src/main/java/grocery/app/MyAccountActivity.java
@@ -1,16 +1,29 @@
 package grocery.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toolbar;
+
+import grocery.app.databinding.ActivityMyAccountBinding;
+import grocery.app.util.WindowBarColor;
 
 public class MyAccountActivity extends AppCompatActivity {
+
+    private ActivityMyAccountBinding binding;
+    private MyAccountActivity activity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_account);
+        WindowBarColor.setColor(this);
+        binding = DataBindingUtil.setContentView(activity, R.layout.activity_my_account);
+        binding.toolbar.setTitle("My Account");
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     public void onBackClick(View view) {
